eae523c090bf2ad36c58204e9c8d300af994fa5b
Danilo Batson <>
2022-01-28 01:29:22 -0800

    docs: adds kevler data

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -1,8 +1,35 @@
-const planets = [
-  {
-    "hello": "world",
-  }
-]
+const fs = require('fs');
+const parse = require('csv-parse');
 
+const habitablePlanets = []
 
-module.exports = planets
+function isHabitablePlanet(planet) {
+  return planet['koi_disposition'] === 'CONFIRMED'
+    && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11
+    && planet['koi_prad'] < 1.6;
+}
+
+fs.createReadStream('kepler_data.csv')
+  .pipe(parse({
+    comment: '#',
+    columns: true,
+}))
+  .on('data', data => {
+    if (isHabitablePlanet(data)) {
+      habitablePlanets.push(data);
+    }
+  })
+  .on('error', err => {
+  console.error(err);
+  })
+  .on('end', () => {
+    console.log(habitablePlanets.map((planet) => {
+    return planet['kepler_name']
+    }))
+    console.log(`${habitablePlanets.length} habitable planets found!`)
+  })
+
+module.exports = {
+  planets: habitablePlanets,
+  getAllPlanets,
+};

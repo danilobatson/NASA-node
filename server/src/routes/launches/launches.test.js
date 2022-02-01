70c6934b2e6627d8ce61b6f7ff30371b822af8c7
Danilo Batson <djbatson19@gmail.com>
2022-02-01 03:03:27 -0800

    feat: adds testing

---
+++ server/src/routes/launches/launches.test.js
@@ -0,0 +1,55 @@
+const request = require('supertest')
+const { mongoConnect, mongoDisconnect} = require('../../services/mongo')
+const app = require('../../app')
+
+describe('Launches API',  () => {
+  beforeAll(async() => {
+    await mongoConnect()
+  })
+
+  afterAll(async () => {
+    await mongoDisconnect()
+  })
+
+  describe('TEST GET /launches', () => {
+    test('It should respond with a 200 success', async () => {
+      const response = await request(app)
+        .get('/launches')
+        .expect('Content-Type', /json/)
+        .expect(200)
+    })
+  })
+
+  describe('TEST POST /launches', () => {
+    const completeLaunchData = {
+      mission: 'USS Enterprise',
+      rocket: 'NCC 1701-D',
+      target: 'Kepler-62 f',
+      launchDate: 'January 4, 2028'
+    }
+
+    const launchDataWithoutDate = {
+      mission: 'USS Enterprise',
+      rocket: 'NCC 1701-D',
+      target: 'Kepler-62 f',
+    }
+
+    const launchDataWithInvalidDate = {
+      mission: 'USS Enterprise',
+      rocket: 'NCC 1701-D',
+      target: 'Kepler-62 f',
+      launchDate: 'foo'
+    }
+
+    test('It should respond with 201 created', async () => {
+      const response = await request(app).post('/launches')
+        .send(completeLaunchData)
+        .expect('Content-Type', /json/)
+        .expect(201)
+      const requestDate = new Date(completeLaunchData.launchDate).valueOf
+      const responseDate = new Date(response.body.launchDate).valueOf
+      expect(responseDate).toBe(requestDate)
+      expect(response.body).toMatchObject(launchDataWithoutDate)
+    })
+  })
+})
